df4e2c896a2155d6f18408e7b70ddb33eced9858
zieadshabkalieh <73721459+zieadshabkalieh@users.noreply.github.com>
2023-05-20 04:19:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://SaadTabban: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@github.com/SaadTabban/pu ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 Professor_Bot.py &")
+os.system("git clone https://zieadshabkalieh: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@github.com/zieadshabkalieh/pu ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 Professor_Bot.py &")
